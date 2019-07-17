ec210f7ceaf69eb3f96408efac514e5b67289531
ogaroh <ogaroerick1998@gmail.com>
2019-07-17 15:54:05 +0300

    'add to favorites' button

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -12,6 +12,7 @@ export class StockItemComponent implements OnInit {
   public price: number;
   public previousPrice: number;
   public positiveChange: boolean;
+  public favorite: boolean; // initially instantiated as 'false'
 
   constructor() { }
 
@@ -20,7 +21,14 @@ export class StockItemComponent implements OnInit {
     this.code = 'AAPL';
     this.price = 120.65;
     this.previousPrice = 101.55;
+
+    // check if current price is greater than or equal to previous price
     this.positiveChange = this.price >= this.previousPrice;
+  }
+
+  toggleFavorite() {
+    console.log("Toggling state for this stock.");
+    this.favorite = !this.favorite;
   }
 
 }
